00d88769d0a45a85afc6ac709bf398bf56ebdb15
komiljon4717 <komiljon4717@gmail.com>
2021-11-06 13:27:36 +0500

    Exercise Grade done

--- homework2.js
+++ homework2.js
@@ -2,13 +2,15 @@
 
 // function FizzBuzz(num){
 //     if(num % 3 == 0){
-//         console.log('Fizz');
+//         process.stdout.write("Fizz");
 //     }
 //     if (num % 5 == 0) {
-//         console.log('Buzz');
+//         process.stdout.write("Buzz");
 //     }
 // }
 // FizzBuzz(15)
+
+
 
 
 
@@ -31,17 +33,48 @@
 // FindPrimeNumber(7)
 
 
-// Star
-function showStar(num){
-    for (let i = 1; i <= num; i++) {
-        for (let j = 1; j <= i; j++) {
-            process.stdout.write(" *"); 
-        }   
-        console.log(' ');  
-    }
-}
-showStar(5)
 
+
+// Star
+// function showStar(num){
+//     for (let i = 1; i <= num; i++) {
+//         for (let j = 1; j <= i; j++) {
+//             process.stdout.write(" *"); 
+//         }   
+//         console.log(' ');  
+//     }
+// }
+// showStar(5)
+
+
+
+// Exercise Grade
+function ExerciseGrade(array){
+    let generalPoint = 0;
+    for (let i = 0; i < array.length; i++) {
+        generalPoint += array[i];
+    }
+    let overBall = generalPoint / array.length
+    
+    if (overBall <= 59) {
+        return console.log('F');
+    }
+    if (overBall <= 69) {
+        return console.log('D');
+    }
+    if (overBall <= 79) {
+        return console.log('C');
+    }
+    if (overBall <= 89) {
+        return console.log('B');
+    }
+    else{
+        console.log('A');
+    }
+    
+}
+let arr = [80, 60, 70, 90]
+ExerciseGrade(arr)
 
 
 
