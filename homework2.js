afc5d6471e1e7baa7c62c292ee29debc7acffc69
komiljon4717 <komiljon4717@gmail.com>
2021-11-06 12:20:26 +0500

    Prime number done

--- homework2.js
+++ homework2.js
@@ -1,11 +1,35 @@
 // FizzBuzz
 
-function FizzBuzz(num){
-    if(num % 3 == 0){
-        console.log('Fizz');
+// function FizzBuzz(num){
+//     if(num % 3 == 0){
+//         console.log('Fizz');
+//     }
+//     if (num % 5 == 0) {
+//         console.log('Buzz');
+//     }
+// }
+// FizzBuzz(15)
+
+
+
+// Prime Number
+
+function FindPrimeNumber(num){
+    let sanoq = 0
+    for (let i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sanoq++;
+        }
     }
-    if (num % 5 == 0) {
-        console.log('Buzz');
+    if (sanoq == 0) {
+        console.log("Prime Number");       
+    }
+    else{
+        console.log("Not Prime Number");
     }
 }
-FizzBuzz(15)
+FindPrimeNumber(7)
+
+
+
+
