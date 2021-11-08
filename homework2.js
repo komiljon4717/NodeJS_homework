1e17efa088fc24c5bfd630ea71a4ad701a0f0f6b
komiljon4717 <komiljon4717@gmail.com>
2021-11-08 09:21:00 +0500

    yuklab qo'ydim

--- homework2.js
+++ homework2.js
@@ -1,3 +1,20 @@
+// Landscape or
+// function WhichOne(a ,b){
+//     if (a > b) {
+//         return console.log("Portrait mode");
+//     }
+//     else{
+//         return console.log("Landscape mode");
+//     }
+// }
+// WhichOne(4, 3)
+
+
+
+
+
+
+
 // FizzBuzz
 
 // function FizzBuzz(num){
@@ -5,12 +22,32 @@
 //         process.stdout.write("Fizz");
 //     }
 //     if (num % 5 == 0) {
-//         process.stdout.write("Buzz");
+//         return process.stdout.write("Buzz");
+//     }
+//     else if (isNaN(num)) {
+//         return console.log('Not a number');
+//     }
+//     else {
+//         console.log(num);
 //     }
 // }
-// FizzBuzz(15)
+// FizzBuzz(7)
 
 
+
+
+
+// Demerit points
+
+
+// function Radar(speed) {
+//     let a = Math.floor(speed / 5)
+    
+//     if (a > 12) {
+//         console.log('Suspended');
+//     }
+// }
+// Radar(70)
 
 
 
@@ -79,16 +116,16 @@
 
 
 // 3 & 5
-// function findCollected(num){
+// function findCollected(arr){
 //     let collected = 0
-//     for (let i = 1; i <= num; i++) {
-//         if (i % 3 == 0 || i % 5 == 0) {
-//             collected += i
+//    for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 3 == 0 || arr[i] % 5 == 0) {
+//             collected += arr[i]
 //         }
-//     }
+//    }
 //     return collected
 // }
-// console.log(findCollected(30));
+// console.log(findCollected([3, 10, 7, 4]));
 
 
 
@@ -98,11 +135,10 @@
 
 // function chack(allData){
 //     for (let i = 0; i < allData.length; i++) {
-//         // if (allData === 'string') {
-//         //     console.log(allData[i]);
-//         // }
+//         if (isNaN(allData[i])) {
+//             console.log(allData[i]);
+//         }
 //     }
-//     console.log(allData);
 // }
 
 
@@ -111,13 +147,6 @@
 //     username: 'Nosirov',
 //     age: 22,
 //     phoneNumber: 1083474
-// }
-
-// for (const key in obj) {
-//     // console.log(key);
-//     for (let i = 0; i < obj.length; i++) {
-//        console.log(key[i]) 
-//     }
 // }
 
 
@@ -137,21 +166,82 @@
 
 
 // showNumber
-function showNumber(num){
-    let odd_num = 0
-    let couple_num = 0
-    for (let i = 1; i <= num; i++) {
-        if (i % 2 == 0) {
-            couple_num++
-        }
-        else{
-            odd_num++
-        }
-    }
-    console.log(odd_num + ' odd number');
-    console.log(couple_num + ' couple number');
-}
-showNumber(20)
+// function showNumber(arr){
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 == 0) {
+//             console.log(arr[i] + ' Juft');
+//         }
+//         else{
+//             console.log(arr[i] + ' Toq');
+//         }
+//     }
+    
+// }
+// showNumber([0, 6, 7, 15])
+
+
+
+// if1
+
+// let a = 5
+// if (a > 0) {
+//     return console.log(a+=1);
+// }
+// else{
+//     return console.log(a);
+// }
+
+
+// if2
+
+
+// let a = -5
+// if (a > 0) {
+//     return console.log(a+=1);
+// }
+// else{
+//     return console.log(a-=2);
+// }
+
+
+// if3
+
+// let a = 0
+// if (a > 0) {
+//     return console.log(a+=1);
+// }
+// else if (a < 0) {
+//     return console.log(a-=2);
+// }
+// else{
+//     return console.log(a +=10);
+// }
+
+
+// if4
+
+// let a = 4
+// let b = 5
+// let c = -8
+
+// let d = 0
+
+// if (a > 0) {
+//     d++
+// }
+// if (b > 0) {
+//     d++
+// }
+// if (c > 0) {
+//     d++
+// }
+// console.log(d + ' musbat');
+
+
+
+
+// if5
+
 
 
 
