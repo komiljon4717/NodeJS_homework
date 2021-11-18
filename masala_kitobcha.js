f477e525a87133925946ab18850bc8d5c4d2a7e9
komiljon4717 <komiljon4717@gmail.com>
2021-11-18 18:40:39 +0500

    Faktarial

---
+++ masala_kitobcha.js
@@ -0,0 +1,15 @@
+/*===========Recursion=========*/
+
+
+/*========faktrial=======*/
+
+// function faktarial(num) {
+//     if (num === 1) {
+//         return 1
+//     }
+//     return num * faktarial(num - 1)
+// }
+
+// console.log(faktarial(6));
+
+/*========faktrial=======*/
