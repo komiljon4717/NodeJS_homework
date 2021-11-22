2802958347e3cd37f299124b5b30bb25385eb87f
komiljon4717 <komiljon4717@gmail.com>
2021-11-22 23:37:06 +0500

    done

--- homework12.js
+++ homework12.js
@@ -1,3 +1,5 @@
+"use srict"
+
 
 /*============symmetrical===================*/
 
@@ -46,18 +48,66 @@
 // find_sum(arr1, arr2, arr3)
 
 
+/*============todo list===================*/
+
+let user_arr = []
+let todo = []
+let id_count = 1
+
+function create_user(username, password) {
+    let obj = {}
+    if (username.length >= 8 && username.length < 32) {
+        obj.username = username
+    }
+    else{
+        console.log("Error login 8 ta belgidan ko'p va 32 ta belgidan kam bo'lishi kerak");
+    }
+
+    if (password.length > 8 && password.length < 32 ) {
+        obj.password = password
+    }
+    else{
+        console.log("Error dang");
+    }
+
+    obj = {
+        user_id: id_count,
+        username,
+        password,
+    }
+    id_count++
+    user_arr.push(obj)
+}
 
 
 
+create_user("johnDoe007", "johnDoe1012")
+create_user("johnUwik007", "johnUwik1012")
+create_user("johnASS007", "johnASS1012")
 
 
+console.log(user_arr);
+todos("johnUwik007", "johnUwik1012", "salom churvaqalar")
 
+let todo_id = 1
+function todos(username, password, todo_txt) {
+    for (let i = 0; i < user_arr.length; i++) {
+        if (user_arr[i].username === username && user_arr[i].password === password) {
+            let obj7 = {
+                // todo_id,
+                todo_txt,
+                user_id: user_arr[i].user_id
+            }
+            // todo_id++
 
+            todo.push(obj7)
+        }
+        
+    }
+    return todo
+}
 
-
-
-
-
+console.log(todo);
 
 
 
