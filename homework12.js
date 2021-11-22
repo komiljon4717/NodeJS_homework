7fca93b2accfa1ab03f56534691e31d47beb2eb5
komiljon4717 <komiljon4717@gmail.com>
2021-11-22 20:37:38 +0500

    symmetric done

---
+++ homework12.js
@@ -0,0 +1,15 @@
+
+// function symmetrical(num) {
+//     let a = num.toString()
+//     let len = Math.floor(a.length / 2)
+//     let first = a.slice(0,len)
+//     let second = a.slice(len)
+
+//     console.log(first === second);
+    
+// }
+
+
+
+
+// symmetrical(1121)
