6ca338f26aeaf4346d24688197a14924ec8a20ac
komiljon4717 <komiljon4717@gmail.com>
2021-11-22 20:43:32 +0500

    hour to seconds done

--- homework12.js
+++ homework12.js
@@ -1,4 +1,6 @@
 
+/*============symmetrical===================*/
+
 // function symmetrical(num) {
 //     let a = num.toString()
 //     let len = Math.floor(a.length / 2)
@@ -10,6 +12,48 @@
 // }
 
 
-
-
 // symmetrical(1121)
+
+
+
+/*============seconds===================*/
+
+// function how_many_seconds(arg) {
+//     let all_sec = arg * 60 * 60
+//     console.log(`${arg} soat, ${all_sec} soniyaga teng`);
+// }
+
+
+// how_many_seconds(2)
+
+
+/*============symmetrical===================*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
