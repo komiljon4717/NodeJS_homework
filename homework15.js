0946425190d75e549913a5171d39b912cdd30b31
komiljon4717 <komiljon4717@gmail.com>
2021-11-25 14:25:23 +0500

    conver done

---
+++ homework15.js
@@ -0,0 +1,18 @@
+
+/*===================convert=================*/
+
+// let char= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
+// function convert(arg) {
+//     arg = arg.toLowerCase()
+//     let safe = ''
+//     for (let i = 0; i < arg.length; i++) {
+//         for (let k = 0; k < char.length; k++) {
+//             if (arg[i] === char[k].toLocaleLowerCase()) {
+//                 safe += k + ' '
+//             }
+//         }
+//     }
+//     console.log(safe);
+// }
+// convert("JAVASCRIPT")
+
