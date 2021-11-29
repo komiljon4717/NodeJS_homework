6acdff7d10678ddf94b00c9f4fd4ccc3629f69c3
komiljon4717 <komiljon4717@gmail.com>
2021-11-29 15:59:40 +0500

    two problem done

---
+++ homework17.js
@@ -0,0 +1,60 @@
+/*=============================================*/
+
+
+// function change(words) {
+//     let new_words = ''
+//     for (let i = 0; i < words.length; i++) {
+//         if (words[i] === words[i].toUpperCase()) {
+//             new_words += words[i].toLowerCase()
+//             console.log(words[i]);
+//         }
+//         else{
+//             new_words += words[i].toUpperCase()
+//         }
+//     }
+//     return new_words
+// }
+// console.log(change("Tez jiggarrang tulki"));
+
+/*================================================*/
+
+// function logger(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         console.log(`${i}-qator`);
+//         for (let k = 0; k < arr[i].length; k++) {
+//             console.log(arr[i][k]);
+            
+//         }
+        
+//     }
+// }
+
+// console.log(logger([[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
