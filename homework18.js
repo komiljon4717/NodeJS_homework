bb474aadd4ad865270e53b49e65dfcc3d766e548
komiljon4717 <komiljon4717@gmail.com>
2021-11-30 20:38:45 +0500

    obj to array done

--- homework18.js
+++ homework18.js
@@ -1,22 +1,25 @@
 /*=========================how much is true=====================*/
-function count_true(params) {
-    let count = 0
-    for (let i = 0; i < params.length; i++) {
-        if (params[i] === true) {
-            count++
-        }
+// function count_true(params) {
+//     let count = 0
+//     for (let i = 0; i < params.length; i++) {
+//         if (params[i] === true) {
+//             count++
+//         }
         
-    }
-    console.log(count);
-}
+//     }
+//     console.log(count);
+// }
 
-count_true([])
+// count_true([])
 
 /*======================================================*/
 
+function obj_to_array(obj) {
+    let result = Object.keys(obj).map((key) => [key, obj[key]])
+    console.log(result);
+}
 
-
-
+obj_to_array({"a": 1, "b": 2, "c": 3})
 
 
 
