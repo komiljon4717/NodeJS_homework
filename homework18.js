5984a09f6bb59c0bc55f0d0be9bc8a2055c413fd
komiljon4717 <komiljon4717@gmail.com>
2021-11-30 20:25:38 +0500

    count true done

---
+++ homework18.js
@@ -0,0 +1,35 @@
+/*=========================how much is true=====================*/
+function count_true(params) {
+    let count = 0
+    for (let i = 0; i < params.length; i++) {
+        if (params[i] === true) {
+            count++
+        }
+        
+    }
+    console.log(count);
+}
+
+count_true([])
+
+/*======================================================*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
