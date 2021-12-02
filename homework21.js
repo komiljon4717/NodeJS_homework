0934534ddbd59d1620b288e87a3edb301f7252e2
komiljon4717 <komiljon4717@gmail.com>
2021-12-02 20:29:55 +0500

    problem done

--- homework21.js
+++ homework21.js
@@ -22,34 +22,46 @@
 // console.log(factorize_numbers(10));
 
 
-/*================factorize_numbers=================*/
+/*================hacker_speak=================*/
 
-function hacker_speak(value) {
-    value = value.toLowerCase()
-    let result = ''
-    for (x = 0; x < value.length; x++){
-        if (value[x] == "a"){
-            result += "4"
-        } else if (value[x] == "s"){
-            result += "5"
-        } else if (value[x] == "e"){
-            result += "3"
-        } else if (value[x] == "i"){
-            result += "1"
-        } else if (value[x] == "o"){
-            result += "0"
-        } else{
-            result += value[x]
-        }
-   }
-   return result
+// function hacker_speak(value) {
+//     value = value.toLowerCase()
+//     let result = ''
+//     for (x = 0; x < value.length; x++){
+//         if (value[x] == "a"){
+//             result += "4"
+//         } else if (value[x] == "s"){
+//             result += "5"
+//         } else if (value[x] == "e"){
+//             result += "3"
+//         } else if (value[x] == "i"){
+//             result += "1"
+//         } else if (value[x] == "o"){
+//             result += "0"
+//         } else{
+//             result += value[x]
+//         }
+//    }
+//    return result
+// }
+
+// console.log(hacker_speak('jAvascript is cool'));
+
+/*================max_min=================*/
+
+function max_min_string(str) {
+    let output = []
+    let arr = str.split(" ")
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = Number(arr[i])
+    }
+    output.push(Math.max(...arr))
+    output.push(Math.min(...arr))
+    str = output.join(' ')
+    console.log(str);
 }
 
-console.log(hacker_speak('jAvascript is cool'));
-
-
-
-
+max_min_string('1 2 -3 5 3 6')
 
 
 
