0b4c77498969ef99c33ec1dff7038afc043886a7
komiljon4717 <komiljon4717@gmail.com>
2021-12-06 21:53:21 +0500

    room status done

---
+++ homework22.js
@@ -0,0 +1,61 @@
+/*=====================chat room status==================*/
+
+// function room_status(array) {
+//     let size = array.length
+
+//     if (size === 0) {
+//         console.log("no one online");
+//     }
+//     else if (size === 1) {
+//         console.log(`${array[0]} online`);
+//     }
+//     else if (size === 2) {
+//         console.log(`${array[0]} and ${array[1]} online`);
+//     }
+//     else if (size > 2) {
+//         console.log(`${array[0]} and ${array[1]} ${size - 2} more online`);
+        
+//     }
+// }
+
+// room_status(["Ali", "Vali", "Eshmat", "Umidjon"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
