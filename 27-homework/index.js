cb73e70cb6874e7a817e982a79a8be1acd2af273
komiljon4717 <komiljon4717@gmail.com>
2021-12-15 17:47:46 +0500

    done

--- 27-homework/index.js
+++ 27-homework/index.js
@@ -1,12 +1,36 @@
-function containsDuplicate(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let k = 0; k < nums.length; k++) {
-            if (nums[i] === nums[k] && i !== k) {
-                return true
-            }
+// function containsDuplicate(nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let k = 0; k < nums.length; k++) {
+//             if (nums[i] === nums[k] && i !== k) {
+//                 return true
+//             }
             
+//         }
+//         return false
+//     }
+// }
+// console.log(containsDuplicate([1,2,3,4]));
+
+
+
+
+
+
+
+
+
+function findDuplicates(nums) {
+    let res = []
+    for (let i = 0; i < nums.length; i++) {
+        for (let l = 0; l < nums.length; l++) {
+            if (nums[i] === nums[l] && i !== l && !res.includes(nums[l])) {
+                res.push(nums[l])
+            }
         }
-        return false
     }
+    return res
 }
-console.log(containsDuplicate([1,2,3,4]));
+
+console.log(findDuplicates([4,5,4,1,2,6]));
+
+
