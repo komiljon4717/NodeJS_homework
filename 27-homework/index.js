1d0802a67dcbd1f35cce85c6eac09cf9b9bffa3f
komiljon4717 <komiljon4717@gmail.com>
2021-12-15 20:43:22 +0500

    leetcode problem

--- 27-homework/index.js
+++ 27-homework/index.js
@@ -19,18 +19,32 @@
 
 
 
-function findDuplicates(nums) {
-    let res = []
-    for (let i = 0; i < nums.length; i++) {
-        for (let l = 0; l < nums.length; l++) {
-            if (nums[i] === nums[l] && i !== l && !res.includes(nums[l])) {
-                res.push(nums[l])
-            }
-        }
+// function findDuplicates(nums) {
+//     let res = []
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let l = 0; l < nums.length; l++) {
+//             if (nums[i] === nums[l] && i !== l && !res.includes(nums[l])) {
+//                 res.push(nums[l])
+//             }
+//         }
+//     }
+//     return res
+// }
+
+// console.log(findDuplicates([4,5,4,1,2,6]));
+
+
+
+/*===============isPalindrome=====================*/
+
+var isPalindrome = function(x) {
+    x = x.toString()
+    let x1 = ''
+    for (let i = x.length - 1; i >= 0; i--) {
+        x1 += x[i]
+        
     }
-    return res
-}
-
-console.log(findDuplicates([4,5,4,1,2,6]));
-
+    return x === x1
+};
+console.log(isPalindrome(121));
 
