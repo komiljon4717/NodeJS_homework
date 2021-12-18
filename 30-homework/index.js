8fb41082cd1efac60182831bd169039ed4edbd50
komiljon4717 <komiljon4717@gmail.com>
2021-12-18 18:21:34 +0500

    remove dublicate

--- 30-homework/index.js
+++ 30-homework/index.js
@@ -1,14 +1,29 @@
+/*===================factor chain=================*/
+// function factor_chain(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let k = i; k < arr.length; k++) {
+//             if (arr[k] % arr[i] === 0) {
+//                 continue;
+//             }
+//             else return false
+//         }
+//     }
+//     return true
+// }
 
-function factor_chain(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let k = i; k < arr.length; k++) {
-            if (arr[k] % arr[i] === 0) {
-                continue;
-            }
-            else return false
-        }
-    }
-    return true
+// console.log(factor_chain([3, 6, 12, 24]));
+
+/*========================================================*/
+
+function removeDublicat(arr) {
+    arr = new Set(arr)
+    return new Array(...arr)
 }
+console.log(removeDublicat(["The", "John", "The"]));
 
-console.log(factor_chain([1, 1, 1, 1, 1, 1]));
+
+
+
+
+
+
