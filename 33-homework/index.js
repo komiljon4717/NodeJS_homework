00e53df8b5c05843156f539a55f4114db38ca9e0
komiljon4717 <komiljon4717@gmail.com>
2021-12-22 17:22:12 +0500

    findNaN done

--- 33-homework/index.js
+++ 33-homework/index.js
@@ -1,7 +1,73 @@
 /*======================uniquesort======================*/
-function uniqueSort(arr) {
-    arr = [...new Set(arr)].sort()
-    return arr
-}
+// function uniqueSort(arr) {
+//     arr = [...new Set(arr)].sort()
+//     return arr
+// }
 
-console.log(uniqueSort([1, 2, 3, 2, 1, 4, 5]));
+// console.log(uniqueSort([1, 2, 3, 2, 1, 4, 5]));
+
+/*========================================================*/
+
+// function findNaN(arr) {
+//     let a = arr.findIndex(Number.isNaN)
+//     console.log(a);
+// }
+// findNaN([1, 2, NaN])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
