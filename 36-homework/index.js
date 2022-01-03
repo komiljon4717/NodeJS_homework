f27c7e2d7b177d380b44e0285556e33d037f0929
komiljon4717 <komiljon4717@gmail.com>
2022-01-03 18:46:17 +0500

    done

--- 36-homework/index.js
+++ 36-homework/index.js
@@ -1,13 +1,19 @@
 
-function shiftToLeft(num1, num2){
-    return num1 << num2
+// function shiftToLeft(num1, num2){
+//     return num1 << num2
+// }
+
+// console.log(shiftToLeft(-6, 5));
+
+/*====================================================================*/
+
+function reversedBinaryInteger(num) {
+    let newNum = num.toString(2).split('').reverse().join('')
+    newNum = parseInt(newNum, 2)
+    console.log(newNum);
 }
 
-console.log(shiftToLeft(-6, 5));
-
-
-
-
+reversedBinaryInteger(10)
 
 
 
