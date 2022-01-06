86611afaf352d6d5195c6557ba5dd4b819837239
komiljon4717 <komiljon4717@gmail.com>
2022-01-06 15:23:53 +0500

    convertToNumber done

--- 37-homework/index.js
+++ 37-homework/index.js
@@ -11,22 +11,29 @@
 
 /*====================================================*/
 
-function expensive_orders(obj, num) {
-    let newObj = {}
+// function expensive_orders(obj, num) {
+//     let newObj = {}
+//     for (const key in obj) {
+//         if (obj[key] >= num) {
+//             newObj[key] = obj[key]
+//         }
+//     }
+//     return newObj
+// }
+
+// console.log(expensive_orders({"a": 500, "b": 1200, "c": 1500}, 1000));
+
+
+/*====================================================*/
+
+function converToNumber(obj) {
     for (const key in obj) {
-        if (obj[key] >= num) {
-            newObj[key] = obj[key]
-        }
+        obj[key] = +obj[key]
     }
-    return newObj
+    return obj
 }
 
-console.log(expensive_orders({"a": 500, "b": 1200, "c": 1500}, 1000));
-
-
-
-
-
+console.log(converToNumber({piano: "300", speaker: "700"}));
 
 
 
