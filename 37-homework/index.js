d6bb544dd19365bd306f73815f9461980cf801a7
komiljon4717 <komiljon4717@gmail.com>
2022-01-06 15:03:43 +0500

    burglar done

---
+++ 37-homework/index.js
@@ -0,0 +1,32 @@
+
+/*===========================================================*/
+
+function burglar_series(obj, key, val) {
+    obj[key] = val
+    return obj
+}
+
+console.log(burglar_series({piano: 500,"Brutus": 400}, "Caligula", 440));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
