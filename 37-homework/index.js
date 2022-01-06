4c47e3089ae6ea6b1ca036c5eb987562c052673d
komiljon4717 <komiljon4717@gmail.com>
2022-01-06 15:19:52 +0500

    expensive orders done

--- 37-homework/index.js
+++ 37-homework/index.js
@@ -1,16 +1,27 @@
 
 /*===========================================================*/
 
-function burglar_series(obj, key, val) {
-    obj[key] = val
-    return obj
+// function burglar_series(obj, key, val) {
+//     obj[key] = val
+//     return obj
+// }
+
+// console.log(burglar_series({piano: 500,"Brutus": 400}, "Caligula", 440));
+
+
+/*====================================================*/
+
+function expensive_orders(obj, num) {
+    let newObj = {}
+    for (const key in obj) {
+        if (obj[key] >= num) {
+            newObj[key] = obj[key]
+        }
+    }
+    return newObj
 }
 
-console.log(burglar_series({piano: 500,"Brutus": 400}, "Caligula", 440));
-
-
-
-
+console.log(expensive_orders({"a": 500, "b": 1200, "c": 1500}, 1000));
 
 
 
